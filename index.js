b6586b0408d59353897a366a4bf35b415edfc67a
gabrielalexsander18 <gabrielfaria257@gmail.com>
2023-09-14 14:21:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('.sort')
+
+const sortNumber = () => {
+    const max =  Math.floor(document.querySelector(".input_max").value)
+    const min = Math.ceil(document.querySelector(".input_min").value)
+    const quantity = document.querySelector(".input_quantity").value
+
+    const Arr = []
+
+    for (var i = 0; i < quantity; i++) {
+
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+         Arr.push(result)
+     }
+        alert(Arr)
+    }
+
+button.addEventListener('click', sortNumber)
