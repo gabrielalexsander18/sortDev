cf27d19af590ff0b7c2657756a6ae887f0964298
gabrielalexsander18 <gabrielfaria257@gmail.com>
2024-08-12 11:06:49 -0300

    feat created time functionality for suspense and numbers drawn on the screen, not as before as an alert.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const button = document.querySelector('.sort')
+const buttonReturn = document.querySelector('#button_return')
 
 const sortNumber = () => {
     const max =  Math.floor(document.querySelector(".input_max").value)
     const min = Math.ceil(document.querySelector(".input_min").value)
     const quantity = document.querySelector(".input_quantity").value
+
+    const rNumero = document.querySelector("#resultadoNumero")
+    const containerOpacity = document.querySelector("#container_opacity")
 
     const Arr = []
     
@@ -16,7 +20,38 @@ const sortNumber = () => {
         
             Arr.push(result)
         }
-            alert(Arr)
-        }
+                containerOpacity.style.display = 'flex',
+
+                    rNumero.innerHTML = `Número: ${Arr}`
+            
     }
-button.addEventListener('click', sortNumber)
+}
+
+
+function mostrarLoader() {
+    // Mostra o spinner
+    document.getElementById("container_loader").style.display = "flex";
+
+    // Simula um tempo de carregamento de 3 segundos
+    setTimeout(function() {
+        // Esconde o spinner
+        document.getElementById("container_loader").style.display = "none";
+
+        sortNumber()
+    }, 5000);
+}
+
+const returnHome = () => {
+    const containerOpacity = document.querySelector("#container_opacity")
+
+    containerOpacity.style.display = 'none'
+
+    recarregarPagina()
+}
+
+function recarregarPagina() {
+    location.reload();
+}
+
+button.addEventListener('click', mostrarLoader)
+buttonReturn.addEventListener('click', returnHome)
